ef764bb89e3359232cd172e8bd89655f5a90b489
FYHenry <frederic_yann.henry@protonmail.com>
2023-05-16 16:38:14 +0200

    FabriquerMur done.

--- src/fr/algorithmie/FabriquerMur.java
+++ src/fr/algorithmie/FabriquerMur.java
@@ -16,8 +16,24 @@ public class FabriquerMur {
         verifier(3, 1, 7, true);
         verifier(1, 1, 7, false);
     }
+
+    /**
+     * Returns "true" if and only if exists a combination of small bricks (1m) and
+     * big bricks (5m) to build a wall of size defined.
+     * Otherwise, "false" is returned.
+     *
+     * @param nbSmall 1 meter brick size
+     * @param nbBig 5 meter brick size
+     * @param longueur total size of the wall
+     * @return "It is buildable" (boolean)
+     */
     static boolean fabriquerMur(int nbSmall, int nbBig, int longueur) {
         boolean resultat = false;
+        for(int bNbr = 0; bNbr < nbBig; bNbr++){
+            for(int sNbr = 0; sNbr < nbSmall; sNbr++){
+                resultat = resultat || bNbr * 5 + sNbr == longueur;
+            }
+        }
         return resultat;
     }
     private static void verifier(int nbSmall, int nbBig, int longueur, boolean b)
